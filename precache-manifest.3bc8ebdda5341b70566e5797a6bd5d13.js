b5a520f04544e407ea3a869dbf21dfc1da4389c2
Alexandra Vlasenko <sandra00798@mail.ru>
2020-09-10 02:15:51 +0300

    Updates

---
+++ precache-manifest.3bc8ebdda5341b70566e5797a6bd5d13.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7739a1135828c8ee5cb43f090c71f1ce",
+    "url": "/bntu/index.html"
+  },
+  {
+    "revision": "d244fa28ee5d60d1db9a",
+    "url": "/bntu/static/css/main.fea5fd13.chunk.css"
+  },
+  {
+    "revision": "534b0ed76de168f617be",
+    "url": "/bntu/static/js/2.29a7c6fd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bntu/static/js/2.29a7c6fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d244fa28ee5d60d1db9a",
+    "url": "/bntu/static/js/main.278663b0.chunk.js"
+  },
+  {
+    "revision": "683c1bd2a656d80c0515",
+    "url": "/bntu/static/js/runtime-main.8766298a.js"
+  },
+  {
+    "revision": "ee642307e498e4aac19c6edd067e2822",
+    "url": "/bntu/static/media/arrow.ee642307.svg"
+  },
+  {
+    "revision": "9471c5a63575847504f93c57eb90a34c",
+    "url": "/bntu/static/media/cop.9471c5a6.png"
+  }
+]);
